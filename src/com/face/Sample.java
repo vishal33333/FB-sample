9737285bf022dd6572a9d2c501119dad61063590
vishal33333 <vinith.visshal@gmail.com>
2021-11-09 19:44:13 +0530

    Password Functionality

--- src/com/face/Sample.java
+++ src/com/face/Sample.java
@@ -1,5 +1,13 @@
 package com.face;
 
 public class Sample {
+	
+	public static void Password() {
+System.out.println("Password");
+	}
+	
+	public static void main(String[] args) {
+		Password();
+	}
 
 }
